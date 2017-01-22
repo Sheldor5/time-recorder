ed81f9b9f85936bb7beda945deddb1273ce35bac
Michael Palata <if15b037@technikum-wien.at>
2017-01-22 03:19:20 +0100

    added XSD, XML and java XML validation

--- tr-core/src/main/java/at/sheldor5/tr/core/rules/RuleLoader.java
+++ tr-core/src/main/java/at/sheldor5/tr/core/rules/RuleLoader.java
@@ -1,7 +1,39 @@
 package at.sheldor5.tr.core.rules;
 
+import java.io.InputStream;
+import java.util.List;
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by Michael Palata <a href="https://github.com/Sheldor5">@github.com/Sheldor5</a> on 21.01.2017.
  */
 public class RuleLoader {
+
+  private static final Logger LOGGER = LogManager.getLogger(RuleLoader.class);
+
+  public static List<Rule> getRules() {
+    return null;
+  }
+
+  static boolean validateAgainstXSD(final InputStream xml, final InputStream xsd) {
+    try {
+      final SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+      final Schema schema = factory.newSchema(new StreamSource(xsd));
+      Validator validator = schema.newValidator();
+      validator.validate(new StreamSource(xml));
+      xml.close();
+      xsd.close();
+      return true;
+    } catch(final Exception generalException) {
+      LOGGER.warn(generalException.getMessage());
+      return false;
+    }
+  }
+
 }
---
+++ tr-core/src/test/java/at/sheldor5/tr/core/rules/RuleLoaderTest.java
@@ -0,0 +1,68 @@
+package at.sheldor5.tr.core.rules;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/**
+ * Created by Michael Palata on 22.01.2017.
+ */
+public class RuleLoaderTest {
+
+  private static File FILE_XSD;
+  private static File FILE_XML;
+  private static File FILE_INVALID_XML_1;
+  private static File FILE_INVALID_XML_2;
+  private static File FILE_INVALID_XML_3;
+  private static File FILE_INVALID_XML_4;
+  private static File FILE_INVALID_XML_5;
+
+  @Before
+  public void init() throws URISyntaxException {
+    FILE_XSD = new File(this.getClass().getResource("/rules/rules.xsd").toURI());
+    FILE_XML = new File(this.getClass().getResource("/rules/austria.xml").toURI());
+    FILE_INVALID_XML_1 = new File(this.getClass().getResource("/rules/invalid_1.xml").toURI());
+    FILE_INVALID_XML_2 = new File(this.getClass().getResource("/rules/invalid_2.xml").toURI());
+    FILE_INVALID_XML_3 = new File(this.getClass().getResource("/rules/invalid_3.xml").toURI());
+    FILE_INVALID_XML_4 = new File(this.getClass().getResource("/rules/invalid_4.xml").toURI());
+    FILE_INVALID_XML_5 = new File(this.getClass().getResource("/rules/invalid_5.xml").toURI());
+  }
+
+  @Test
+  public void test_valid_xml() throws IOException {
+    final FileInputStream xsd = new FileInputStream(FILE_XSD);
+    final FileInputStream xml = new FileInputStream(FILE_XML);
+    Assert.assertTrue("XML should successfully be validated", RuleLoader.validateAgainstXSD(xml, xsd));
+  }
+
+  @Test
+  public void test_invalid_xmls() throws IOException {
+    FileInputStream xsd;
+    FileInputStream xml;
+
+    xsd = new FileInputStream(FILE_XSD);
+    xml = new FileInputStream(FILE_INVALID_XML_1);
+    Assert.assertFalse("XML should be invalid", RuleLoader.validateAgainstXSD(xml, xsd));
+
+    xsd = new FileInputStream(FILE_XSD);
+    xml = new FileInputStream(FILE_INVALID_XML_2);
+    Assert.assertFalse("XML should be invalid", RuleLoader.validateAgainstXSD(xml, xsd));
+
+    xsd = new FileInputStream(FILE_XSD);
+    xml = new FileInputStream(FILE_INVALID_XML_3);
+    Assert.assertFalse("XML should be invalid", RuleLoader.validateAgainstXSD(xml, xsd));
+
+    xsd = new FileInputStream(FILE_XSD);
+    xml = new FileInputStream(FILE_INVALID_XML_4);
+    Assert.assertFalse("XML should be invalid", RuleLoader.validateAgainstXSD(xml, xsd));
+
+    xsd = new FileInputStream(FILE_XSD);
+    xml = new FileInputStream(FILE_INVALID_XML_5);
+    Assert.assertFalse("XML should be invalid", RuleLoader.validateAgainstXSD(xml, xsd));
+  }
+}
